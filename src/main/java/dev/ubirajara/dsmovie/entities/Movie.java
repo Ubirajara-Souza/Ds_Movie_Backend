4292924fb552032f24cf9053432a7ec21f0424c3
Ubirajara Souza <bira.souza2015@hotmail.com>
2022-03-10 18:18:52 -0300

    Domain model, database seed

---
+++ src/main/java/dev/ubirajara/dsmovie/entities/Movie.java
@@ -0,0 +1,22 @@
+package dev.ubirajara.dsmovie.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_movie")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private Double score;
+    private Integer count;
+    private String image;
+
+}
---
+++ src/main/java/dev/ubirajara/dsmovie/entities/Score.java
@@ -0,0 +1,30 @@
+package dev.ubirajara.dsmovie.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_score")
+public class Score {
+
+    @EmbeddedId
+    private ScorePK id = new ScorePK();
+    private Double value;
+
+    public void setMovie(Movie movie) {
+        id.setMovie(movie);
+    }
+
+    public void setUser(User user) {
+        id.setUser(user);
+    }
+}
